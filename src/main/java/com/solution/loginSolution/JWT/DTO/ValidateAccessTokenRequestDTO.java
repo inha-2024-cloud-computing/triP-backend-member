24822ea077d3557280d4e08b54abc42f8809f155
seate <86824703+seate@users.noreply.github.com>
2024-06-18 21:37:23 +0900

    fix: valitedateAccessTokenRequestDTO no args constructor implement

--- src/main/java/com/solution/loginSolution/JWT/DTO/ValidateAccessTokenRequestDTO.java
+++ src/main/java/com/solution/loginSolution/JWT/DTO/ValidateAccessTokenRequestDTO.java
@@ -3,8 +3,10 @@ package com.solution.loginSolution.JWT.DTO;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ValidateAccessTokenRequestDTO {
     @NotBlank
